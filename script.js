d116eb8a50a92d93dd8922e2c4f8b7234850869f
Adnene_wsl <adnen.guessoum@gmail.com>
2022-10-17 17:09:21 +0200

    Add first basic operation functions

---
+++ script.js
@@ -0,0 +1,20 @@
+// basic calc functions
+
+const add = function(a, b) {
+	return a+b;
+};
+
+const subtract = function(a, b) {
+	return a-b;	
+};
+
+const multiply = function(a, b) {
+	return a*b;	
+};
+
+const divide = function(a, b) {
+	return a/b;	
+};
+
+
+
