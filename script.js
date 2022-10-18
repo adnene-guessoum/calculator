d94c880b1d7e62798e2bf180b95774e75db974c3
Adnene_wsl <adnen.guessoum@gmail.com>
2022-10-18 20:59:14 +0200

    Display pressed key and store value inside an array

--- script.js
+++ script.js
@@ -20,5 +20,25 @@ function operate(operator, a, b){
 	return operator(a, b);
 }
 
-// button grid 
+// function populating the calc display
+const display = document.querySelector("#display-calc");
+console.log(display);
+const operationArray = [];
 
+
+const keys = document.querySelectorAll(".keys");
+
+
+
+const keyPress = keys.forEach(
+	key => key.addEventListener(
+		'click', function(e) {
+			const userInput = e.target.id ;
+			display.innerText = userInput;	
+			operationArray.push(userInput);
+			return userInput;
+}
+	)
+);
+
+console.log(operationArray);
